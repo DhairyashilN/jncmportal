618c28737567ba934af3446b8f0049d14cfc6913
DhairyashilN <dhairyashil.nalawade@gmail.com>
2017-06-25 19:39:23 +0530

    welocme.blade modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,54 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-            .content {text-align: center;}
-            .title {font-size: 84px;}
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="head-block">
-                <div class="row">
-                    <div class="col-lg-12 header">
-                        <h2>Jay Navnath Engineering Works</h2>
-                    </div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+        .head-block{margin: 150px 0px;}
+        .content {text-align: center;font-weight: bold;}
+        .title {font-size: 80px;font-weight: bold;}
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="head-block">
+            <div class="row">
+                <div class="col-lg-12 page-header">
+                <h2 class="content title">Jay Navnath </h2>
+                <h1 class="content">Engineering Works</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                <h3 class="content">Customer Record Portal</h3>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
