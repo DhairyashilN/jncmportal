6d985e0ea670f79d9e4ad8f4ab3c77fc8b8721b8
DhairyashilN <dhairyashil.nalawade@gmail.com>
2017-06-29 10:24:54 +0530

    welcome.blade file changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>{{ config('app.name')}} | Home</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Latest compiled and minified CSS -->
@@ -45,7 +45,7 @@
 
             <div class="row">
                 <div class="col-lg-12">
-                <h3 class="content">Customer Record Portal</h3>
+                <h3 class="content">Customer Record & Enquiry Management Portal</h3>
                 </div>
             </div>
         </div>
